0b7d3c5536eee41dff8dc607c1e56223bcd8eeab
Rick Battipede <rbattipede@gmail.com>
2019-06-11 01:59:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ var recipes = {}
 function updateObjectWithKeyAndValue(object, key, value){ var newRecipes = Object.assign({}, object, {[key]: value});
   return newRecipes;
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object;
+}
